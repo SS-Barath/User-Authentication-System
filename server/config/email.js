ee8d09b975cd0cd7a07fee8ab923daaee26c6c98
SS-Barath <barathsaravanan07@email.com>
2026-04-16 00:36:47 +0530

    Updated Code files

--- server/config/email.js
+++ server/config/email.js
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
@@ -10,4 +10,4 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export default transporter;
+module.exports = transporter;
