3e19643845c8a70b4449a4e9a4fc029ecbd49c5c
SS-Barath <barathsaravanan07@email.com>
2026-04-16 01:40:36 +0530

    Update Server.js file

--- server/server.js
+++ server/server.js
@@ -1,36 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./config/connection");
-const protect = require("./middleware/authMiddleware");
 
 const authRoutes = require("./routes/authRoutes");
-
 require("dotenv").config();
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+    origin: "https://user-auth-client.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("Backend API Running!");
 });
 
 const PORT = process.env.PORT || 4000;
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 sequelize.authenticate()
     .then(async () => {
         console.log("Database connected using Sequelize");
-
         await sequelize.sync({});
         console.log("Tables Synced");
-
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
-        });
     })
     .catch((error) => {
         console.error("Unable to connect to database: ", error);
